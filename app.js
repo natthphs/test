fec956777f71cd1f3772021f59873cbab48c090f
natthphs <natthphs@ais.co.th>
2023-05-10 19:45:34 +0700

    1.0.0

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/test',function(req,res){
+    res.end('hello')
+})
+
+const server = app.listen(8081,function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
